10d432f6598417faa1b7341c62c9893e8fa0e7b5
Valentin <kushnarevval@gmail.com>
2023-04-25 18:29:25 +0400

    25.04.2023

--- Cafe.js
+++ Cafe.js
@@ -1,119 +1,143 @@
-// Виды кофе:
-// 1. Американо
-// 2. Каппучино
+// Type of coffee:
+// 1. Americano
+// 2. Cappuccino
 
-// Размер стаканчика:
+// Size of cup:
 // 1. Small
 // 2. Medium
 // 3. Big
 
-// Рецепт американо:
-// 1. Помолоть зерна
-// 2. Налить воду в кофеварку
-// 3. Помолотые зерна насыпьте в кофеварку
-// 4. Собрать кофеварку (гейзерную) и поставить на плиту
-// 5. Налить кофе в стакан
+// Recipe americano:
+// 1. Grind arabica beans in a coffee grinder
+// 2. Pour water into Bialetti
+// 3. Pour arabica beans into Bialetti
+// 4. Collect Bialetti and put on the stove
+// 5. Pour coffee into a glass
 
-// Рецепт каппучино:
-// 1. Помолоть зерна
-// 2. Налить воду в кофеварку
-// 3. Помолотые зерна насыпьте в кофеварку
-// 4. Собрать кофеварку (гейзерную) и поставить на плиту
-// 5. Налить кофе в стакан
-// 6. Добавить молоко
+// Recipe cappuccino:
+// 1. Grind arabica beans in a coffee grinder
+// 2. Pour water into Bialetti
+// 3. Pour arabica beans into Bialetti
+// 4. Collect Bialetti and put on the stove
+// 5. Pour coffee into a glass
+// 6. Add milk to coffee
 
 
-///// Ингридиенты для кофе
+// Border
+console.log('===============================================')
+
+// Welcome message
+console.log('Welcome to the cafe!')
+
+
+///// Sandwich coffee
 let coffeeMaker = "Bialetti";
-let liquid1 = "воду";
-let liquid2 = "молоко";
-let coffee = "зерна арабики";
+let liquid1 = "water";
+let liquid2 = "milk";
+let coffee = "arabica beans";
+
+///// Recipe of coffee
+let stepOfCoffee1 = `1. Grind ${coffee} in a coffee grinder`;
+let stepOfCoffee2 = `2. Pour ${liquid1} into ${coffeeMaker}`;
+let stepOfCoffee3 = `3. Pour ${coffee} into ${coffeeMaker}`;
+let stepOfCoffee4 = `4. Collect ${coffeeMaker} and put on the stove`;
+let stepOfCoffee5 = `5. Pour coffee into a glass`;
+let stepOfCoffee6 = `6. Add ${liquid2} to coffee`;
 
 
-///// Рецепт кофе
-let stepOfCoffee1 = `1. Перемолоть ${coffee} в кофемолке`;
-let stepOfCoffee2 = `2. Налить ${liquid1} в ${coffeeMaker}`;
-let stepOfCoffee3 = `3. Молотые ${coffee} насыпать в ${coffeeMaker}`;
-let stepOfCoffee4 = `4. Собрать ${coffeeMaker} и поставить на плиту`;
-let stepOfCoffee5 = `5. Налить кофе в стакан`;
-let stepOfCoffee6 = `6. Добавить в кофе ${liquid2}`;
 
-
-///// Цена кофе
-class PriceCoffee {
-    constructor(name) {
-      this.name = name;
-    }
-    getPriceCoffee(sizeOfCup) {
-        let priceMessage;
-        if (sizeOfCup == 1) {
-          priceMessage = "\nЦена вашего напитка: 100 руб.";
-        } else if (sizeOfCup == 2) {
-          priceMessage = "\n\nЦена вашего напитка: 120 руб.";
-        } else if (sizeOfCup == 3) {
-          priceMessage = "\n\nЦена вашего напитка: 150 руб.";
-        } else {
-          priceMessage = "\n\nВведите верно размер стаканчика!";
-        }
-        return console.log(priceMessage);
-      }
-  }
-  
-  let priceCup = new PriceCoffee('Маленький стаканчик');
-  priceCup.getPriceCoffee(1); // 1 - маленький стаканчик, 2 - средний стаканчик, 3 - большой стаканчик
-
-
-///// Вид кофе
+///// Type of coffee
 class Coffee {
   constructor(name) {
     this.name = name;
+    this.topping1 = liquid1;
+    this.topping3 = liquid2;
+
   }
+
   getRecipeCoffee(typeOfCoffee) {
     let recipeMessage;
-    if (typeOfCoffee == 1) {
-      recipeMessage = `\nВы выбрали американо!\n\nРецепт приготовления напитка:\n${stepOfCoffee1}\n${stepOfCoffee2}\n${stepOfCoffee3}\n${stepOfCoffee4}\n${stepOfCoffee5}`;
-    } else if (typeOfCoffee == 2) {
-      recipeMessage = `\nВы выбрали каппучино!\n\nРецепт приготовления напитка:\n${stepOfCoffee1}\n${stepOfCoffee2}\n${stepOfCoffee3}\n${stepOfCoffee4}\n${stepOfCoffee5}\n${stepOfCoffee6}`;
+    if (typeOfCoffee == 'americano') {
+      recipeMessage = `\nYou choose americano.\n\nDrink recipe:\n${stepOfCoffee1}\n${stepOfCoffee2}\n${stepOfCoffee3}\n${stepOfCoffee4}\n${stepOfCoffee5}`;
+    } else if (typeOfCoffee == 'cappuccino') {
+      recipeMessage = `\nYou choose cappuccino.\n\nDrink recipe:\n${stepOfCoffee1}\n${stepOfCoffee2}\n${stepOfCoffee3}\n${stepOfCoffee4}\n${stepOfCoffee5}\n${stepOfCoffee6}`;
     }
     return console.log(recipeMessage);
   }
+
+  getPriceCoffee(sizeOfCup) {
+    let priceMessage;
+    if (sizeOfCup == 'small') {
+      priceMessage = "\nPrice: 100 Rub.";
+    } else if (sizeOfCup == 'medium') {
+      priceMessage = "\n\nPrice: 120 Rub.";
+    } else if (sizeOfCup == 'big') {
+      priceMessage = "\n\nPrice: 150 Rub.";
+    } else {
+      priceMessage = "\n\nYou may choose only one of three size of cup: small, medium or big!";
+    }
+    return console.log(priceMessage);
+  }
 }
 
-let coffee1 = new Coffee('Американо');
-coffee1.getRecipeCoffee(1); // 1 - американо, 2 - каппучино
+
+let coffee1 = new Coffee('cappuccino');
+coffee1.getRecipeCoffee('cappuccino'); 
+coffee1.getPriceCoffee('small'); 
 
 
-///// Ингридиенты для сэндвича
-let bread = "ломтик хлеба";
-let topping1 = "лист салата";
-let topping2 = "ветчину";
-let topping3 = "сыр";
+// Border
+console.log('===============================================')
 
-///// Рецепт сэндвича
-let stepOfSandwich1 = `1. Взять ${bread}`;
-let stepOfSandwich2 = `2. Положить ${topping1}`;
-let stepOfSandwich3_1 = `3. Положить ${topping2}`;
-let stepOfSandwich3_2 = `3. Положить сверху ${topping3}`;
 
-///// Вид сэндвича
-  class Sandwich {
-    constructor(name) {
-      this.name = name;
-    }
-    getRecipeSandwich(typeOfSandwich) {
-      let recipeMessage;
-      if (typeOfSandwich == 1) {
-        recipeMessage = `\nВы выбрали сэндвич с ветчиной!\n\nРецепт приготовления:\n${stepOfSandwich1}\n${stepOfSandwich2}\n${stepOfSandwich3_1}`;
-      } else if (typeOfSandwich == 2) {
-        recipeMessage = `\nВы выбрали сэндвич с сыром!\n\nРецепт приготовления:\n${stepOfSandwich1}\n${stepOfSandwich2}\n${stepOfSandwich3_2}`;
-      }
-      return console.log(recipeMessage);
-    }
+///// Sandwich ingredients
+let bread = "slice of bread";
+let topping1 = "salad leaf";
+let topping2 = "ham";
+let topping3 = "cheese";
+
+///// Recipe of sandwich
+let stepOfSandwich1 = `1. Get ${bread}`;
+let stepOfSandwich2 = `2. Put ${topping1}`;
+let stepOfSandwich3_1 = `3. Put ${topping2}`;
+let stepOfSandwich3_2 = `3. Put ${topping3} on top`;
+
+///// Type of sandwich
+class Sandwich {
+  constructor(name, topping1, topping2) {
+    this.name = name;
+    this.topping1 = topping1;
+    this.topping1 = topping2;
   }
-  
-  let sandwich1 = new Sandwich('Сэндвич с ветчиной');
-    sandwich1.getRecipeSandwich(1); // 1 - сэндвич с ветчиной, 2 - сэндвич с сыром
 
+  getRecipeSandwich(typeOfSandwich) {
+    let recipeMessage;
+    if (typeOfSandwich == 'ham sandwich') {
+      recipeMessage = `\nYou choose ham sandwich.\n\nRecipe:\n${stepOfSandwich1}\n${stepOfSandwich2}\n${stepOfSandwich3_1}`;
+    } else if (typeOfSandwich == 'cheese sandwich') {
+      recipeMessage = `\nYou choose cheese sandwich.\n\nRecipe:\n${stepOfSandwich1}\n${stepOfSandwich2}\n${stepOfSandwich3_2}`;
+    }
+    return console.log(recipeMessage);
+  }
 
-  ////// Итоговая цена заказа
+  getPriceSandwich(typeOfSandwich) {
+    let priceMessage;
+    if (typeOfSandwich == 'ham sandwich') {
+      priceMessage = "\nPrice: 150 Rub.";
+    } else if (typeOfSandwich == 'cheese sandwich') {
+      priceMessage = "\n\nPrice: 120 Rub.";
+    } else {
+      priceMessage = "\n\nYou may choose only one of two types of sandwich: ham sandwich, cheese sandwich!";
+    }
+    return console.log(priceMessage);
+  }
+}
+
+let sandwich1 = new Sandwich('ham sandwich', 'salad leaf', 'ham');
+sandwich1.getRecipeSandwich('cheese sandwich');
+sandwich1.getPriceSandwich('cheese sandwich'); 
+
+// Border
+console.log('===============================================')
+
 
